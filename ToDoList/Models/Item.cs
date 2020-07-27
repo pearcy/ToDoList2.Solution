a0597cc5a91df21504c2713f461287113eb28093
Joseph Pearce <joseph.pearce05@gmail.com>
2020-07-27 10:49:31 -0700

    add ToDoList project, add code to retrieve objects from database
    Co-authored-by: Kevin Davis <thekidnamedkd@gmail.com>

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set; }
+    public int Id { get; }
+   
+    public Item(string description)
+    {
+      Description = description;
+    }
+
+     public static List<Item> GetAll()
+    {
+        List<Item> allItems = new List<Item> { };
+        MySqlConnection conn = DB.Connection();
+        conn.Open();
+        MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+        cmd.CommandText = @"SELECT * FROM items;";
+        MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+        while (rdr.Read())
+        {
+            int itemId = rdr.GetInt32(0);
+            string itemDescription = rdr.GetString(1);
+            Item newItem = new Item(itemDescription, itemId);
+            allItems.Add(newItem);
+        }
+        conn.Close();
+        if (conn != null)
+        {
+            conn.Dispose();
+        }
+        return allItems;
+    }
+
+    // public static List<Item> GetAll()
+    // {
+    //   return _instances;
+    // }
+
+    public static void ClearAll()
+    {
+     
+    }
+
+    public static Item Find(int searchId)
+    {
+       // Temporarily returning placeholder item to get beyond compiler errors until we refactor to work with database.
+    Item placeholderItem = new Item("placeholder item");
+    return placeholderItem;
+      
+    }
+
+  }
+}
